e7e88a60e95c0843c4f47038096c92a51766a567
ocioman <120775542+ocioman@users.noreply.github.com>
2025-09-14 22:55:30 +0200

    modifica appbar ed errori

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "840e76fc09b7fbc6915a94a1667b9831",
+const RESOURCES = {"flutter_bootstrap.js": "3f005b360ce698dfa3a2706fb4d9fb1b",
 "version.json": "4624a4a8ef861a87718789cfe467c71f",
 "index.html": "792be2a5a26cc1215228f4d88bab412f",
 "/": "792be2a5a26cc1215228f4d88bab412f",
-"main.dart.js": "749bf721c6584a67217819a9b7cf44e7",
+"main.dart.js": "e5883f9c5849c6aaac56d398568e6a9f",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
